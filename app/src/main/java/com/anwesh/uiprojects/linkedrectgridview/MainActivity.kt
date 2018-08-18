9fff435e38a3e31a1ef526bc7b6a2455ec930aa4
Anwesh43 <anweshthecool0@gmail.com>
2018-08-19 02:57:17 +0530

    adding RectGridView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectgridview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectgridview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrectgridview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectgridview.RectGridView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectGridView.create(this)
     }
 }
