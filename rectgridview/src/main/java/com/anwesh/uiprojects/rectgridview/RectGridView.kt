01ea05805206243c186b9e10a8f043772e82b811
Anwesh43 <anweshthecool0@gmail.com>
2018-08-19 01:36:33 +0530

    importing libraries and defining nodes

--- rectgridview/src/main/java/com/anwesh/uiprojects/rectgridview/RectGridView.kt
+++ rectgridview/src/main/java/com/anwesh/uiprojects/rectgridview/RectGridView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.rectgridview
 /**
  * Created by anweshmishra on 19/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+
+val nodes : Int = 5 
