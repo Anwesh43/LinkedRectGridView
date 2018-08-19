6dd75581606914b547aeedb38727e220bbf75340
Anwesh43 <anweshthecool0@gmail.com>
2018-08-19 15:36:50 +0530

    adding onAnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectgridview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectgridview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedrectgridview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.rectgridview.RectGridView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RectGridView.create(this)
+        val view : RectGridView = RectGridView.create(this)
         fullScreen()
+        view.addOnAnimationListener({createToast("${it + 1} animation completed")}, {createToast("${it + 1} animation is reset")})
+    }
+
+    private fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
